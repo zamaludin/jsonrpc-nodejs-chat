39d16a09634bb9832757734be609d5991420076b
unknown <zamaludin25823@yahoo.com>
2017-01-10 01:06:46 +0700

    first

---
+++ public/js/chat.js
@@ -0,0 +1,358 @@
+// This file is executed in the browser, when people visit /chat/<random id>
+
+$(function(){
+
+	// getting the id of the room from the url
+	var id = Number(window.location.pathname.match(/\/chat\/(\d+)$/)[1]);
+
+	// connect to the socket
+	var socket = io();
+
+	// variables which hold the data for each person
+	var name = "",
+		email = "",
+		img = "",
+		friend = "";
+
+	// cache some jQuery objects
+	var section = $(".section"),
+		footer = $("footer"),
+		onConnect = $(".connected"),
+		inviteSomebody = $(".invite-textfield"),
+		personInside = $(".personinside"),
+		chatScreen = $(".chatscreen"),
+		left = $(".left"),
+		noMessages = $(".nomessages"),
+		tooManyPeople = $(".toomanypeople");
+
+	// some more jquery objects
+	var chatNickname = $(".nickname-chat"),
+		leftNickname = $(".nickname-left"),
+		loginForm = $(".loginForm"),
+		yourName = $("#yourName"),
+		yourEmail = $("#yourEmail"),
+		hisName = $("#hisName"),
+		hisEmail = $("#hisEmail"),
+		chatForm = $("#chatform"),
+		textarea = $("#message"),
+		messageTimeSent = $(".timesent"),
+		chats = $(".chats");
+
+	// these variables hold images
+	var ownerImage = $("#ownerImage"),
+		leftImage = $("#leftImage"),
+		noMessagesImage = $("#noMessagesImage");
+
+
+	// on connection to server get the id of person's room
+	socket.on('connect', function(){
+
+		socket.emit('load', id);
+	});
+
+	// save the gravatar url
+	socket.on('img', function(data){
+		img = data;
+	});
+
+	// receive the names and avatars of all people in the chat room
+	socket.on('peopleinchat', function(data){
+
+		if(data.number === 0){
+
+			showMessage("connected");
+
+			loginForm.on('submit', function(e){
+
+				e.preventDefault();
+
+				name = $.trim(yourName.val());
+
+				if(name.length < 1){
+					alert("Please enter a nick name longer than 1 character!");
+					return;
+				}
+
+				email = yourEmail.val();
+
+				if(!isValid(email)) {
+					alert("Please enter a valid email!");
+				}
+				else {
+
+					showMessage("inviteSomebody");
+
+					// call the server-side function 'login' and send user's parameters
+					socket.emit('login', {user: name, avatar: email, id: id});
+				}
+
+			});
+		}
+
+		else if(data.number > 0) {
+
+			showMessage("personinchat",data);
+
+			loginForm.on('submit', function(e){
+
+				e.preventDefault();
+
+				name = $.trim(hisName.val());
+
+				if(name.length < 1){
+					alert("Please enter a nick name longer than 1 character!");
+					return;
+				}
+
+				if(name == data.user){
+					alert("There already is a \"" + name + "\" in this room!");
+					return;
+				}
+				email = hisEmail.val();
+
+				if(!isValid(email)){
+					alert("Wrong e-mail format!");
+				}
+				else {
+					socket.emit('login', {user: name, avatar: email, id: id});
+				}
+
+			});
+		}
+
+		//else {
+		//	showMessage("tooManyPeople");
+		//}
+
+	});
+
+	// Other useful
+
+	socket.on('startChat', function(data){
+		console.log(data);
+		if(data.boolean && data.id == id) {
+
+			chats.empty();
+
+			if(name === data.users[0]) {
+
+				showMessage("youStartedChatWithNoMessages",data);
+			}
+			else {
+
+				showMessage("heStartedChatWithNoMessages",data);
+			}
+
+			chatNickname.text(friend);
+		}
+	});
+
+	socket.on('leave',function(data){
+
+		if(data.boolean && id==data.room){
+
+			showMessage("somebodyLeft", data);
+			chats.empty();
+		}
+
+	});
+
+	socket.on('tooMany', function(data){
+
+		if(data.boolean && name.length === 0) {
+
+			showMessage('tooManyPeople');
+		}
+	});
+
+	socket.on('receive', function(data){
+
+		showMessage('chatStarted');
+		var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+		var decodedString = Base64.decode(data.msg);
+		console.log(decodedString);
+		if(decodedString == '::readed::'){
+			console.log('kebaca readded');
+			$('.me > .readed').attr('style', 'visibility: visible;');
+		} else
+		if(data.msg.trim().length) {
+			createChatMessage(decodedString, data.user, data.img, moment());
+			scrollToBottom();
+		}
+	});
+
+	textarea.keypress(function(e){
+
+		// Submit the form on enter
+
+		if(e.which == 13) {
+			e.preventDefault();
+			chatForm.trigger('submit');
+		}
+
+
+	});
+	textarea.focus(function(e){
+
+		// Submit the form on enter
+		var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+		var encodedString = Base64.encode('::readed::');
+		socket.emit('msg', {msg: encodedString, user: name, img: img});
+
+
+	});
+
+	chatForm.on('submit', function(e){
+
+		e.preventDefault();
+
+		// Create a new chat message and display it directly
+
+		showMessage("chatStarted");
+
+		if(textarea.val().trim().length) {
+			createChatMessage(textarea.val(), name, img, moment());
+			scrollToBottom();
+
+			// Send the message to the other person in the chat
+			var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+			var encodedString = Base64.encode(textarea.val());
+			socket.emit('msg', {msg: encodedString, user: name, img: img});
+
+		}
+		// Empty the textarea
+		textarea.val("");
+	});
+
+	// Update the relative time stamps on the chat messages every minute
+
+	setInterval(function(){
+
+		messageTimeSent.each(function(){
+			var each = moment($(this).data('time'));
+			$(this).text(each.fromNow());
+		});
+
+	},60000);
+
+	// Function that creates a new chat message
+
+	function createChatMessage(msg,user,imgg,now){
+
+		var who = '';
+
+		if(user===name) {
+			who = 'me';
+		}
+		else {
+			who = 'you';
+		}
+
+		var li = $(
+			'<li class=' + who + '>'+
+				'<div class="image">' +
+					'<img src=' + imgg + ' />' +
+					'<b></b>' +
+					'<i class="timesent" data-time=' + now + '></i> ' +
+
+				'</div>' +
+				'<p></p>' +
+				'<img src="/img/check.png" alt="Smiley face" height="16" width="16" style="visibility: hidden;" class="readed">'+
+			'</li>');
+
+		// use the 'text' method to escape malicious user input
+		li.find('p').text(msg);
+		li.find('b').text(user);
+
+		chats.append(li);
+
+		messageTimeSent = $(".timesent");
+		messageTimeSent.last().text(now.fromNow());
+	}
+
+	function scrollToBottom(){
+		$("html, body").animate({ scrollTop: $(document).height()-$(window).height() },1000);
+		//socket.emit('msg', {msg: '::readed::', user: name, img: img});
+	}
+
+	function isValid(thatemail) {
+
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(thatemail);
+	}
+
+	function showMessage(status,data){
+
+		if(status === "connected"){
+
+			section.children().css('display', 'none');
+			onConnect.fadeIn(1200);
+		}
+
+		else if(status === "inviteSomebody"){
+
+			// Set the invite link content
+			$("#link").text(window.location.href);
+
+			onConnect.fadeOut(1200, function(){
+				inviteSomebody.fadeIn(1200);
+			});
+		}
+
+		else if(status === "personinchat"){
+
+			onConnect.css("display", "none");
+			personInside.fadeIn(1200);
+
+			chatNickname.text(data.user);
+			ownerImage.attr("src",data.avatar);
+		}
+
+		else if(status === "youStartedChatWithNoMessages") {
+
+			left.fadeOut(1200, function() {
+				inviteSomebody.fadeOut(1200,function(){
+					noMessages.fadeIn(1200);
+					footer.fadeIn(1200);
+				});
+			});
+
+			friend = data.users[1];
+			noMessagesImage.attr("src",data.avatars[1]);
+		}
+
+		else if(status === "heStartedChatWithNoMessages") {
+
+			personInside.fadeOut(1200,function(){
+				noMessages.fadeIn(1200);
+				footer.fadeIn(1200);
+			});
+
+			friend = data.users[0];
+			noMessagesImage.attr("src",data.avatars[0]);
+		}
+
+		else if(status === "chatStarted"){
+
+			section.children().css('display','none');
+			chatScreen.css('display','block');
+		}
+
+		else if(status === "somebodyLeft"){
+
+			leftImage.attr("src",data.avatar);
+			leftNickname.text(data.user);
+
+			section.children().css('display','none');
+			footer.css('display', 'none');
+			left.fadeIn(1200);
+		}
+
+		else if(status === "tooManyPeople") {
+
+			section.children().css('display', 'none');
+			tooManyPeople.fadeIn(1200);
+		}
+	}
+
+});
